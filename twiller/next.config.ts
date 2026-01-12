931b3553cf8de7d4ddf5b857766631dd398f173e
w_vivek <wvivek.181@gmail.com>
2026-01-12 12:31:41 +0530

    Deploy frontend to GitHub Pages

--- twiller/next.config.ts
+++ twiller/next.config.ts
@@ -2,20 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-
-  // repo name
   basePath: "/Twiller-Clone",
-
-  // VERY IMPORTANT for GitHub Pages
-  assetPrefix: "/Twiller-Clone/",
-
-  images: {
-    unoptimized: true,
-  },
-
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  assetPrefix: "/Twiller-Clone",
+  images: { unoptimized: true },
+  eslint: { ignoreDuringBuilds: true },
 };
 
 export default nextConfig;
